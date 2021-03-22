804bcd9ec4702016dcc28ebfb18110f09b1c96d7
paw1470 <pawel1470@yahoo.com>
2021-03-22 05:16:13 +0100

    Add get, set, click for fields

--- bot.js
+++ bot.js
@@ -13,3 +13,62 @@ var $winMessageField = $('#double_your_btc_bet_win');
 var $payoutMultiplierField = $('#double_your_btc_payout_multiplier');
 var $winChance = $('#double_your_btc_win_chance');
 
+
+//set, get, click
+function getPayoutMultiplier(){
+    return $payoutMultiplierField.val();
+}
+
+function setPayoutMultiplier(value){
+    $payoutMultiplierField.val(value);
+    $payoutMultiplierField.trigger("keyup");
+}
+
+function getWinChance(){
+    return $winChance.val();
+}
+
+function setWinChance(value){
+    $winChance.val(value);
+    $winChance.trigger("keyup");
+}
+
+function getWinProfit(){
+    return $winProfitField.text();
+}
+
+//bet amount
+function getBetAmount(){
+    return $betAmountField.val();
+}
+
+function setBetAmount(value){
+    $betAmountField.val(value.toFixed(8));
+}
+
+function clickHalfBet(){
+    $buttonHalfBet.trigger("click");
+}
+
+function click2xBet(){
+    $button2xBet.trigger("click");
+}
+
+function clickMinBet(){
+    $buttonMinBet.trigger("click");
+}
+
+    //alert need click on ok button
+// function clickMaxBet(){
+//     $buttonMaxBet.trigger("click");
+// }
+
+function multiplyBet(multiplier){ 
+    var currentValue = getBetAmount();
+    var newValue = (currentValue * multiplier);	
+    setBetAmount(newValue);
+}
+
+function getBalance(){ 
+    return $balanceField.text();
+}
--- tests.js
+++ tests.js
@@ -1,9 +1,14 @@
 function testAll(){
     testPresenceId();
+    testPayoutMultiplierAndWinChance();
+    testBetAmount();
+    testWinProfit();
 }
 
 
-
+function doubleToInt(double){
+    return (double*100000000).toFixed(0);
+}
 
 function testPresenceId(){
     console.log("Button LO is");
@@ -40,3 +45,56 @@ function isPresent(field){
     }
 }
 
+function testPayoutMultiplierAndWinChance(){
+    var multiplier = getPayoutMultiplier();
+    var winChance = getWinChance();
+    setWinChance(30);
+    console.log("Is multiplier changed after change win chance")
+    console.log(multiplier != getPayoutMultiplier());
+    winChance = getWinChance();
+    console.log("Is win chance changed after change multiplier")
+    setPayoutMultiplier(2.5);
+    console.log(winChance != getWinChance());
+    console.log("Back to default");
+    setPayoutMultiplier(2);
+    console.log(getPayoutMultiplier() == 2 && getWinChance() == "47.50%");
+}
+
+function testWinProfit(){
+    clickMinBet();
+    console.log("Is minimal profit 1");
+    console.log(doubleToInt(getWinProfit()) == 1);
+    console.log("Is minimal profit 2 after click 2x");
+    click2xBet();
+    console.log(doubleToInt(getWinProfit()) == 2);
+    console.log("Is minimal profit not 3 after click 2x");
+    console.log(doubleToInt(getWinProfit()) != 3);
+}
+
+function testBetAmount(){
+    clickMinBet();
+    console.log("Is Bet Amount 1");
+    console.log(doubleToInt(getBetAmount()) == 1);
+    console.log("Is Bet Amount 2 after click 2x");
+    click2xBet();
+    console.log(doubleToInt(getBetAmount()) == 2);
+    console.log("Is Bet Amount 4 after click 2x");
+    click2xBet();
+    console.log(doubleToInt(getBetAmount()) == 4);
+    console.log("Is Bet Amount not 3");
+    console.log(doubleToInt(getWinProfit()) != 3);
+    console.log("Is Bet Amount 2 after click /2");
+    clickHalfBet();
+    console.log(doubleToInt(getBetAmount()) == 2);
+            // alert need click on ok button
+    // console.log("Is Bet Amount max after click max");
+    // clickMaxBet;
+    // console.log(doubleToInt(getBetAmount()) == getBalance());
+    //
+    console.log("Insert bet amount value");
+    setBetAmount(0.00000003);
+    console.log(doubleToInt(getBetAmount()) == 3);
+    console.log("bet multiplier");
+    multiplyBet(2);
+    console.log(doubleToInt(getBetAmount()) == 6);
+}
